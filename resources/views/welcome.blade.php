70b905572fd6de935b8f2d89488955b8b81839da
demenkov_as <demenkov_as>
2020-07-13 11:09:47 +0300

    delete comment from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,10 +88,6 @@
                     <a href="/games">Games</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://opencritic.com">OpenCritic</a>
-                    <!-- <a href="https://blog.laravel.com">Blog</a> -->
-                    <!-- <a href="https://nova.laravel.com">Nova</a> -->
-                    <!-- <a href="https://forge.laravel.com">Forge</a> -->
-                    <!-- <a href="https://vapor.laravel.com">Vapor</a> -->
                     <a href="https://github.com/AnotherOneGit/">GitHub</a>
                 </div>
             </div>
